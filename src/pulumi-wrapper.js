35d9d75f4ae3e372f8f76037732402a5c5f7687e
Mitko Tschimev <mitko@dev-thought.cool>
2020-01-29 23:51:04 +0400

    feat: spawn can take stdin now
    - the spawn of pulumi binary uses correct inheritance now

--- src/pulumi-wrapper.js
+++ src/pulumi-wrapper.js
@@ -6,11 +6,7 @@ const { resolve } = require('path');
 
 const execName = process.platform === 'win32' ? 'pulumi.exe' : './pulumi';
 const command = resolve(__dirname, '..', 'tools', 'pulumi', execName);
-const pulumi = spawn(command, process.argv.slice(2), { cwd: process.cwd() });
-
-pulumi.stdout.pipe(process.stdout);
-pulumi.stderr.pipe(process.stderr);
-pulumi.on('error', function(err) {
-  console.error(`Received an error while executing the Pulumi binary: ${err}`);
-  process.exit(1);
+spawn(command, process.argv.slice(2), {
+  cwd: process.cwd(),
+  stdio: 'inherit'
 });
