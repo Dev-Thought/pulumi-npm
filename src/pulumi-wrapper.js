eff3d07ec3a9b7667e8b4e4d4768430c7a4b4c92
Mitko Tschimev <mitko@dev-thought.cool>
2020-02-04 09:38:12 +0400

    fix: pulumi binaries
    - pulumi binaries were not found

--- src/pulumi-wrapper.js
+++ src/pulumi-wrapper.js
@@ -5,8 +5,10 @@ const { spawn } = require('child_process');
 const { resolve } = require('path');
 
 const execName = process.platform === 'win32' ? 'pulumi.exe' : './pulumi';
-const command = resolve(__dirname, '..', 'tools', 'pulumi', execName);
+const binPath = resolve(__dirname, '..', 'tools', 'pulumi');
+const command = resolve(binPath, execName);
 spawn(command, process.argv.slice(2), {
   cwd: process.cwd(),
-  stdio: 'inherit'
+  stdio: 'inherit',
+  env: { ...process.env, PATH: `${process.env.PATH}:${binPath}` }
 });
