c3ecd260de4914c83f1d3ca989c6d5d530ce9b01
Mitko Tschimev <mitko@dev-thought.cool>
2020-05-12 22:55:57 +0400

    feat: update to v2 of pulumi

--- src/pulumi-version.js
+++ src/pulumi-version.js
@@ -4,6 +4,6 @@ const pulumiRepo = 'https://api.github.com/repos/pulumi/pulumi';
 
 module.exports = async function getLatestPulumiVersion() {
   const tags = await fetch(`${pulumiRepo}/tags`).then(res => res.json());
-  const latestVersion1Tag = tags.filter(tag => tag.name.startsWith('v1'))[0];
-  return latestVersion1Tag.name;
+  const latestVersion2Tag = tags.filter(tag => tag.name.startsWith('v2'))[0];
+  return latestVersion2Tag.name;
 };
