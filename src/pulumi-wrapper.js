35b80f0c7ac9261049473385a0fbb00a092624cb
Anh-Vu Nguyen <anhvunguyen@mimacom.com>
2021-04-09 15:34:15 +0200

    Fixed windows install pulumi
    Pointed windows file at .zip instead since that's what pulumi offers for windows,
    renamed TAR_FILE to ZIPPED_FILE,
    updated EXEC_DIR to also include Pulumi/bin, because that's what's coming out of the zip,
    updated binPath to also include Pulumi/bin, because that's what's coming out of the zip,
    added onezip as dependency to handle unzipping of .zip files, when its' process.environment is windows-specific.

--- src/pulumi-wrapper.js
+++ src/pulumi-wrapper.js
@@ -4,8 +4,14 @@
 const { spawn } = require('child_process');
 const { resolve } = require('path');
 
-const execName = process.platform === 'win32' || process.platform === 'win64' ? 'pulumi.exe' : './pulumi';
-const binPath = resolve(__dirname, '..', 'tools', 'pulumi');
+const execName =
+  process.platform === 'win32' || process.platform === 'win64'
+    ? 'pulumi.exe'
+    : './pulumi';
+const binPath =
+  process.platform === 'win32' || process.platform === 'win64'
+    ? resolve(__dirname, 'tools', 'Pulumi', 'bin')
+    : resolve(__dirname, 'tools', 'pulumi');
 const command = resolve(binPath, execName);
 spawn(command, process.argv.slice(2), {
   cwd: process.cwd(),
