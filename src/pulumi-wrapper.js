8594e751e296ecdb8bef3f37ea1d92064fe57a69
Mitko Tschimev <mitko@dev-thought.cool>
2021-05-18 16:03:04 +0400

    fix: wrong binary path
    - fix correct path to downloaded binary

--- src/pulumi-wrapper.js
+++ src/pulumi-wrapper.js
@@ -4,14 +4,15 @@
 const { spawn } = require('child_process');
 const { resolve } = require('path');
 
+const toolsDirectory = resolve(__dirname, '..', 'tools');
 const execName =
   process.platform === 'win32' || process.platform === 'win64'
     ? 'pulumi.exe'
     : './pulumi';
 const binPath =
   process.platform === 'win32' || process.platform === 'win64'
-    ? resolve(__dirname, 'tools', 'Pulumi', 'bin')
-    : resolve(__dirname, 'tools', 'pulumi');
+    ? resolve(toolsDirectory, 'Pulumi', 'bin')
+    : resolve(toolsDirectory, 'pulumi');
 const command = resolve(binPath, execName);
 spawn(command, process.argv.slice(2), {
   cwd: process.cwd(),
